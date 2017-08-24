1bdbec182811378aa8d809aa59ef81316c68bb48
Jerome Kuebler <jkuebler981@gmail.com>
2017-08-24 09:01:28 -0600

    003

--- src/003.go
+++ src/003.go
@@ -1,3 +1,5 @@
+// Also spits out a slice that contains all of the factors of x, because I needed to learn slices
+
 package main
 
 import (
@@ -18,15 +20,17 @@ func is_prime(x int) bool {
 func main(){
 
 	x := 600851475143
+	factor_list := []int{}
 	max_prime := 0
 	max_num := math.Pow(float64(x), 0.5)
-	fmt.Println(max_num)
 	for i := 0; i < int(max_num); i++{
 		if math.Mod(float64(x), float64(i)) == 0{
+			factor_list = append(factor_list, i)
 			if is_prime(i){
 				max_prime = i
 			}
 		}
 	}
 	fmt.Println(max_prime)
+	fmt.Println(factor_list)
 }
