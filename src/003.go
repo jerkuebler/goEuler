d00d5b9fee364d2151f88975f60bba95ac856b9b
Jerome Kuebler <jkuebler981@gmail.com>
2017-08-24 08:17:53 -0600

    003

--- src/003.go
+++ src/003.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+func is_prime(x int) bool {
+
+	max_num := math.Pow(float64(x), 0.5)
+	for i := 2; i < int(max_num); i++ {
+		if math.Mod(float64(x), float64(i)) == 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func main(){
-	j := 0.0
 
-	j += 1.1
-
-	fmt.Println(j)
+	x := 600851475143
+	max_prime := 0
+	max_num := math.Pow(float64(x), 0.5)
+	fmt.Println(max_num)
+	for i := 0; i < int(max_num); i++{
+		if math.Mod(float64(x), float64(i)) == 0{
+			if is_prime(i){
+				max_prime = i
+			}
+		}
+	}
+	fmt.Println(max_prime)
 }
